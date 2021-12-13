53532213a096f70891afbaa4a66aacaa01baf4aa
MANISH <MANISH@DESKTOP-NPV50CU>
2021-12-13 18:45:07 +0530

    by toni

---
+++ GitDemo303/src/A.java
@@ -0,0 +1,33 @@
+
+public class A 
+{
+	
+	
+	public static void main(String[] args)
+	{
+	
+		A a1=new A();
+		a1.m1();
+		
+	
+			
+		// TODO Auto-generated method stub
+
+	}
+
+	public void m1() 
+	{
+		
+		System.out.println(" M1 of A your program ");
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
